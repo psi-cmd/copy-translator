c1aea6e8b8f006f6b8bf49f9e17b994618839f3c
psi-cmd <psi-cmd@outlook.com>
2022-09-04 03:03:44 +0800

    add multiline support

--- src/mouse.rs
+++ src/mouse.rs
@@ -5,14 +5,27 @@ pub struct MouseState {
 
 impl MouseState {
     pub fn new() -> Self {
+        println!("MouseState::new");
         Self { last_event: 0 }
     }
 
     pub fn down(&mut self) {
+        println!("MouseState::down");
         self.last_event = 1
     }
 
+    pub fn down_middle(&mut self) {
+        println!("MouseState::down_middle");
+        self.last_event = 4
+    }
+
+    pub fn down_right(&mut self) {
+        println!("MouseState::down_right");
+        self.last_event = 6
+    }
+
     pub fn moving(&mut self) {
+        // println!("MouseState::moving");
         match self.last_event {
             1 => self.last_event = 2,
             2 => self.last_event = 2,
@@ -21,14 +34,49 @@ impl MouseState {
     }
 
     pub fn release(&mut self) {
+        println!("MouseState::release");
         match self.last_event {
             2 => self.last_event = 3,
             _ => self.last_event = 0,
         }
     }
 
+    pub fn release_middle(&mut self) {
+        println!("MouseState::release_middle");
+        match self.last_event {
+            4 => self.last_event = 5,
+            _ => self.last_event = 0,
+        }
+    }
+
+    pub fn release_right(&mut self) {
+        println!("MouseState::release_right");
+        match self.last_event {
+            6 => self.last_event = 7,
+            _ => self.last_event = 0,
+        }
+    }
+
     pub fn is_select(&mut self) -> bool {
         if self.last_event == 3 {
+            self.last_event = 0;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn middle_clicked(&mut self) -> bool {
+        if self.last_event == 5 {
+            self.last_event = 0;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn right_clicked(&mut self) -> bool {
+        if self.last_event == 7 {
             self.last_event = 0;
             true
         } else {
